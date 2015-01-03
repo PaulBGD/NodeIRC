2bf3286511d73a333747c7b398d1d0032e2730a4
Paul Sauve <paul@paulbgd.me>
2015-01-03 16:47:26 -0600

    less logging

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var nw = new NwBuilder({
     files: './**/**', // use the glob format
     platforms: ['osx32', 'osx64', 'win32', 'win64', 'linux32', 'linux64']
 });
-
-nw.on('log',  console.log);
+nw.on('log',  function() {
+    // logging is annoying, due to the thousands of lines. let's not
+});
 
 nw.build().then(function () {
     console.log('Finished compiling NodeIRC');
